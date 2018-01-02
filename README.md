d37e35b2c056d4620d7369077bba77a08642f1db
Xu Liangzhan <xlz26296x@163.com>
2018-01-02 21:21:32 +0800

    update 1.0.0

---
+++ README.md
@@ -0,0 +1,24 @@
+# xe-command 操作函数: 支持复制到剪贴板、打印..
+
+## 通过NPM安装最新版本
+
+``` shell
+npm install xe-command --save
+```
+
+### 按需引入
+``` shell
+import { copy } from 'xe-command'
+
+copy('复制该内容到剪贴板')
+```
+
+### 引入所有
+``` shell
+import * as XECommand from 'xe-command'
+
+XECommand.copy('复制该内容到剪贴板')
+```
+
+## License
+Copyright (c) 2017-present, Xu Liangzhan
---
+++ index.js
@@ -0,0 +1,33 @@
+
+function getContainer () {
+  var $copy = document.getElementById('$XECopy')
+  if (!$copy) {
+    $copy = document.createElement('input')
+    $copy.id = '$XECopy'
+    $copy.style.width = '2px'
+    $copy.style.position = 'fixed'
+    $copy.style.left = '-1000px'
+    $copy.style.top = '-1000px'
+    document.body.appendChild($copy)
+  }
+  return $copy
+}
+
+export function copy (content) {
+  var $copy = getContainer()
+  try {
+    $copy.value = content
+    $copy.select()
+    document.execCommand('copy', false, null)
+  } catch (e) {
+    console.log('该浏览器不支持Copy')
+  }
+}
+
+export function print () {
+  try {
+    document.execCommand('print')
+  } catch (e) {
+    window.print()
+  }
+}
