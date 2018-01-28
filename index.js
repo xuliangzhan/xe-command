88eef8a8d624c1e69f2be94fccfd96f5f4e3ffaa
Xu Liangzhan <xlz26296x@163.com>
2018-01-28 15:11:04 +0800

    update 1.0.5

--- index.js
+++ index.js
@@ -1,38 +1,4 @@
+import * as XECommand from './src/command'
 
-function getContainer () {
-  var $copy = document.getElementById('$XECopy')
-  if (!$copy) {
-    $copy = document.createElement('input')
-    $copy.id = '$XECopy'
-    $copy.style.width = '2px'
-    $copy.style.position = 'fixed'
-    $copy.style.left = '-1000px'
-    $copy.style.top = '-1000px'
-    document.body.appendChild($copy)
-  }
-  return $copy
-}
-
-export function copy (content) {
-  var $copy = getContainer()
-  try {
-    $copy.value = content
-    $copy.select()
-    document.execCommand('copy', false, null)
-  } catch (e) {
-    console.log('该浏览器不支持Copy')
-  }
-}
-
-export function print () {
-  try {
-    document.execCommand('print')
-  } catch (e) {
-    window.print()
-  }
-}
-
-export default {
-  copy: copy,
-  print: print
-}
+export * from './src/command'
+export default XECommand
---
+++ src/command.js
@@ -0,0 +1,46 @@
+
+function getContainer () {
+  var $copy = document.getElementById('$XECopy')
+  if (!$copy) {
+    $copy = document.createElement('input')
+    $copy.id = '$XECopy'
+    $copy.style.width = '2px'
+    $copy.style.position = 'fixed'
+    $copy.style.left = '-1000px'
+    $copy.style.top = '-1000px'
+    document.body.appendChild($copy)
+  }
+  return $copy
+}
+
+/**
+ * 复制文本到剪贴板
+ *
+ * @param {String} content 文本内容
+ */
+export function copy (content) {
+  var $copy = getContainer()
+  try {
+    $copy.value = content
+    $copy.select()
+    document.execCommand('copy', false, null)
+  } catch (e) {
+    console.log('该浏览器不支持Copy')
+  }
+}
+
+/**
+ * 调用打印
+ */
+export function print () {
+  try {
+    document.execCommand('print')
+  } catch (e) {
+    window.print()
+  }
+}
+
+export default {
+  copy: copy,
+  print: print
+}
